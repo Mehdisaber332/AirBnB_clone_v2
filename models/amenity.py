480315487dfe7baaa15200b7927cbf3583e59240
Mehdisaber332 <133115021+Mehdisaber332@users.noreply.github.com>
2023-12-20 23:35:22 +0100

    rewrite amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    """Amenity class"""
+    """ Amenity class defined """
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
+
         __tablename__ = "amenities"
+
         name = Column(String(128), nullable=False)
+
     else:
         name = ""
