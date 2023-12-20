16c157663e59cfb97dd6563789f6bd0ea5df527c
Mehdisaber332 <133115021+Mehdisaber332@users.noreply.github.com>
2023-12-20 23:30:40 +0100

    state modified

--- models/state.py
+++ models/state.py
@@ -2,7 +2,6 @@
 """ State Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
-from models.city import City
 from sqlalchemy import Column, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -23,11 +22,13 @@ class State(BaseModel, Base):
     else:
         name = ""
 
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            list_city = []
-            allcities = models.storage.all(City).values()
-            for city in allcities:
-                if city.state_id == self.id:
-                    list_city.append(city)
-            return list_city
+            """
+            getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id => It will be the
+            FileStorage relationship between State and City
+            """
+            return [city for city in models.storage.all("City").values()
+                    if city.state_id == self.id]
