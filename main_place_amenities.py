f054b616b0a56fcd73124a15f5387d15ce667a52
Mehdisaber332 <133115021+Mehdisaber332@users.noreply.github.com>
2023-12-20 23:59:09 +0100

    final update

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 """ Test link Many-To-Many Place <> Amenity
 """
-from models.user import User
-from models.place import Place
+from models import *
 from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models import storage
 
 # creation of a State
 state = State(name="California")
