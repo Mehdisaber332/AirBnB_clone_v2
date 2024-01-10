3a5a0f23771c216652e44b3c385047df5b5c5266
Mehdisaber332 <mehdisaber332@gmail.com>
2024-01-10 03:53:50 -0800

    setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# sets up web servers for the deployment of web_static
+apt-get update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<h1>Hello World</h1>" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+config_content="
+server{
+    listen 80 default_server;
