2fffb0174baaa2f79068feb1085be4f5f9994510
Imad Jhabli <imadjhabli@gmail.com>
2024-01-11 16:01:56 +0100

    change the ip addresses in the codes to your web01 and web02 and execute it because my fabric doesnt work, codes are working tho

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Fabric script (based on the file 2-do_deploy_web_static.py)
+that creates and distributes an archive to your web servers
+"""
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
+import os
+
+
+env.hosts = ['3.90.81.20', '54.84.14.76']
+def do_pack():
+    """Function to generate a .tgz archive from web_static folder."""
+    local("sudo mkdir -p versions")
+    gndate = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(gndate)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """deploys archive to server"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
+
+
+def deploy():
+    """ do path and do the deploy"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
