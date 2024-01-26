02a0dbf4a91012bc1afa0556bc559a9e8790a02b
Mehdisaber332 <mehdisaber332@gmail.com>
2024-01-26 02:40:20 -0800

    add comments

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,22 +8,36 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def home():
+    """
+    display “Hello HBNB!”
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    """
+    display "HBNB"
+    """
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
+    """
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
     return f"C {text.replace('_', ' ')}"
 
 
 @app.route('/python', strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text="is cool"):
+    """
+    display “Python ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
     return f"Python {text.replace('_', ' ')}"
 
 
